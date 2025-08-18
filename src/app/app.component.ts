565b064d6bef90d29f5028b079629f5b11b280b9
Kilian <kilianheyne@icloud.com>
2025-08-18 16:46:24 +0200

    added material button and tooltip for user-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,22 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, RouterLink],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'simple-crm';
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
