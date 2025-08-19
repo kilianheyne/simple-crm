cb63b3c889ef309dc3772db3967fe1ed45a7be09
Kilian <kilianheyne@icloud.com>
2025-08-19 19:45:14 +0200

    added a form-container to dialog-add-user-component

--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
