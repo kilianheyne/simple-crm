626b3fa85b4327bfa6adb2286c94620b84bdc843
Kilian <kilianheyne@icloud.com>
2025-08-23 11:48:09 +0200

    rewriting saveNewUser-function to add a user to firestore database

--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
@@ -7,22 +7,38 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, FormsModule],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+  ],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
 })
 export class DialogAddUserComponent {
-
   newUser: User = new User();
   birthDate!: Date;
+  private firestore = inject(Firestore);
 
   constructor() {}
 
-  saveNewUser() {
+  async saveNewUser() {
     this.newUser.birthDate = this.birthDate.getTime();
-    console.log('Current user is: ', this.newUser)
+
+    try {
+      const userRef = collection(this.firestore, 'users');
+      const docRef = await addDoc(userRef, { ...this.newUser });
+      console.log('User gespeichert mit der ID: ', docRef.id);
+    } catch {
+      console.error('Fehler beim Speichern: ', Error);
+    }
   }
 }
