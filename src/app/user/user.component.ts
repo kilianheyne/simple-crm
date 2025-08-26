10a18c1362fde5a60774e29dffd54444d8492ea6
Kilian <kilianheyne@icloud.com>
2025-08-26 15:28:06 +0200

    users now get rendered from firebase

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from "@angular/material/icon";
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -6,17 +6,27 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
 import { User } from '../models/user.class';
 import { MatCardModule } from '@angular/material/card';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, DialogAddUserComponent, MatCardModule],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, DialogAddUserComponent, MatCardModule, AsyncPipe],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
   readonly dialog = inject(MatDialog);
+  private firestore = inject(Firestore);
   newUser: User = new User();
+  users$!: Observable<User[]>;
 
+
+  ngOnInit():void {
+    const userCollection = collection(this.firestore, 'users');
+    this.users$ = collectionData(userCollection, { idField: 'id'}) as Observable<User[]>;
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
