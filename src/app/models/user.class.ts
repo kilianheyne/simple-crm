f48665c895187a5a69d32d6f4154902f702194cd
Kilian <kilianheyne@icloud.com>
2025-08-28 18:27:48 +0200

    expanded dialog to include an email-input

--- src/app/models/user.class.ts
+++ src/app/models/user.class.ts
@@ -1,6 +1,7 @@
 export class User {
     firstName: string;
     lastName: string;
+    email: string;
     birthDate: number;
     street: string;
     houseNumber: number;
@@ -10,6 +11,7 @@ export class User {
     constructor(obj?: any){
         this.firstName = obj ? obj.firstName : '';
         this.lastName = obj ? obj.lastName : '';
+        this.email = obj? obj.email : '';
         this.birthDate = obj ? obj.birthDate : '';
         this.street = obj ? obj.street : '';
         this.houseNumber = obj ? obj.houseNumber : '';
