dd433b07a0592ad21561d35cd5c6e92292f3f7c1
Kilian <kilianheyne@icloud.com>
2025-09-01 16:55:41 +0200

    user id now gets extracted from URL

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { MatCard } from "@angular/material/card";
+import { Component, inject, OnInit, DestroyRef } from '@angular/core';
+import { MatCard, MatCardModule } from "@angular/material/card";
+import { ActivatedRoute } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [MatCard],
+  imports: [MatCard, MatCardModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
 export class UserDetailComponent {
+
+  userId!: string;
+  private route = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(params => {
+        this.userId = params.get('id')!;
+        console.log('User-ID aus URL:', this.userId);
+      })
+  }
 
 }
